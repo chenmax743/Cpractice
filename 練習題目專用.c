af893ee4935c4ab60c9c748cf3f24669956d0383
chenmax743 <chenmax38@gmail.com>
2024-01-10 15:16:11 +0800

    add practice

---
+++ 練習題目專用.c
@@ -0,0 +1,15 @@
+#include<stdio.h>;
+
+int main()
+{
+    int integer1;
+    int integer2;
+    int sum;
+    printf("enter the first integer1:");
+    scanf("%d",&integer1);
+    printf("enter the integer2:");
+    scanf("%d",&integer2);
+    sum = integer1+integer2;
+    printf("Sum is %d\n",sum);
+    return 0;
+}
