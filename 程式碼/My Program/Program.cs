f0122c54ed9423290d37034f46ee239cf459c745
chenmax743 <chenmax38@gmail.com>
2024-04-29 14:00:28 +0800

    RN12

---
+++ 程式碼/My Program/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace My_PROGRAM
+{
+    class My_PROGRAM
+    {
+        static void Main(string[]args)
+        {
+            int number1;
+            int number2;
+
+            Console.Write("Input a number: ");
+
+            number1 =Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Input a second number: ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = number1+number2;
+            Console.WriteLine("The result is "+ result);
+
+            Console.ReadKey();
+        }
+    }
+    
+}
