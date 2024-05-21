2fc2f32bd5a83c406109398f50ed57e87bdd74df
chenmax743 <chenmax38@gmail.com>
2024-05-21 14:10:56 +0800

    RN16

---
+++ 程式碼/C#程式碼/AreaCalculator/AreaCalculator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ShapeArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer;
+            float result;
+            Console.WriteLine("今天要測量什麼?，直角行案'r',其他按任何案件");
+
+            answer = Console.ReadLine();
+
+            if(answer == "r")
+            {
+                Console.WriteLine("輸入直角的長度");
+                float height = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("輸入直角寬度");
+                float width = float.Parse(Console.ReadLine());
+
+                result = height / width;
+            }
+            else
+            {
+                Console.WriteLine("輸入圓形直徑");
+                float radius = float.Parse(Console.ReadLine());
+
+                result = (float)Math.PI * (radius * radius);
+            }
+
+            Console.WriteLine("測量結果為"+result);
+            Console.ReadLine();
+
+            
+        }
+    }
+}
---
+++ 程式碼/C#程式碼/GuessGame/GuessGame/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace GuessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isCorrectGuess=false;
+            Random random = new Random();
+            int randomnum=random.Next(1,11);
+
+            Console.WriteLine("歡迎來到猜數字遊戲");
+            Console.WriteLine("猜一個一到十的數字");
+
+            Console.WriteLine(randomnum);
+
+            while(!isCorrectGuess)
+            {
+                Console.WriteLine("猜一個數字");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if(guess>randomnum)
+                {
+                    Console.WriteLine("太大了");
+                }
+                else if(guess<randomnum)
+                {
+                    Console.WriteLine("太小了");
+                }
+                else
+                {
+                    Console.WriteLine("恭喜猜對");
+                    isCorrectGuess = true;
+                }
+                
+                
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
