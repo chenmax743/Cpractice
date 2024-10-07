992bef5aa2ffd8faaa4a6569cd1847e3bbe9b426
chenmax743 <84305789+chenmax743@users.noreply.github.com>
2024-10-07 10:17:51 +0800

    RN28

--- 程式碼/C#程式碼/Prac_Variable/Program.cs
+++ 程式碼/C#程式碼/Prac_Variable/Program.cs
@@ -10,77 +10,29 @@ namespace Prac_Variable
     {
         static void Main(string[] args)
         {
-            //string username;
-
-            //console.writeline("請輸入您的大名 :");
-            //username = console.readline();
-            //console.writeline(username + "您好!");
-            //console.readkey();
-
-            //int i1 = 100;
-            //int i2 = 200;
-            //bool result;
-            //result = (i1 > i2);
-
-            //Console.WriteLine(result.ToString());
-
-            //Console.ReadKey();
-
-            //Console.WriteLine("輸入第一個數");
-            //int iNum1 = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("輸入第二個數");
-            //int iNum2 = Convert.ToInt32(Console.ReadLine());
-
-            //if (iNum1 > iNum2)
-            //{
-            //    Console.WriteLine("比較大的數是" + iNum1);
-            //}
-            //else
-            //{
-            //    Console.WriteLine("比較大的數是" + iNum2);
-            //}
-            //Console.ReadKey();
-
-            //do
-            //{
-            //    Console.WriteLine("輸入西元紀年");
-            //    int iNum1 = Convert.ToInt32(Console.ReadLine());
-
-
-            //    if (iNum1 % 400 == 0)
+            //    int[] Score = new int[] { 68,173,19,524};
+            //    Console.WriteLine("排序前");
+            //    foreach (int score in Score)
             //    {
-            //        Console.WriteLine(iNum1 + "閨年");
+            //        Console.WriteLine($"{score,4}");
             //    }
-            //    else if (iNum1 % 100 == 0)
+            //    Console.WriteLine("排序後");
+            //    Array.Sort(Score);
+            //    Array.Reverse(Score); 
+            //    foreach(int score in Score)
             //    {
-            //        Console.WriteLine(iNum1 + "非閨年");
+            //        Console.WriteLine($"{score,4}");
             //    }
-            //    else if (iNum1 % 4 == 0)
-            //    {
-            //        Console.WriteLine(iNum1 + "是閨年");
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine($"{iNum1}不是閨年");
-            //    }
-            //} while (true);
+            //    Console.WriteLine();
 
-            //Console.WriteLine("101到200偶數累加為");
-            //int iSum = 0;
-            //int i = 101;
+            //    int Index = Array.IndexOf(Score,19);
+            //    Console.WriteLine("元素19,Insex=" + Index);
 
-            //while(i<=200)
-            //{
-            //    if(i%2==0)
-            //    {
-            //        iSum += i;
-            //    }
-            //    i++;
-            //}
-            //Console.WriteLine(iSum);
+            int[,]Num= new int [3,4];
 
 
-            Console.ReadKey();
+            Console.ReadLine();
+            
 
         }
     }
