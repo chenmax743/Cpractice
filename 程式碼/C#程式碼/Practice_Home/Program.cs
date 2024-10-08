ece511c65823502852ed2c78b04de8e7a7810956
chenmax743 <chenmax38@gmail.com>
2024-10-08 15:00:38 +0800

    RN30

---
+++ 程式碼/C#程式碼/Practice_Home/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Home
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random dice = new Random();
+            int iRoll1=dice.Next(1,7);
+            int iRoll2=dice.Next(1,7);
+            int iRoll3=dice.Next(1,7);
+
+            int total = iRoll1 + iRoll2 + iRoll3;
+
+            
+
+            Console.WriteLine($"Dice Roll :{iRoll1}+{iRoll2}+{iRoll3}={total}");
+
+            if (total > 14)
+            {
+                Console.WriteLine("You Win");
+            }
+            if (total < 15)
+            {
+                Console.WriteLine("You Lose");
+            }
+            
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
