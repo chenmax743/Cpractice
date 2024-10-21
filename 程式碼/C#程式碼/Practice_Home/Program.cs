64f2c65262aef215d23f53b1fbad7a697c9a4002
chenmax743 <chenmax38@gmail.com>
2024-10-21 15:34:15 +0800

    RN33

--- 程式碼/C#程式碼/Practice_Home/Program.cs
+++ 程式碼/C#程式碼/Practice_Home/Program.cs
@@ -11,31 +11,101 @@ namespace Practice_Home
     {        
         static void Main(string[] args)
         {
-            int[] numbers = { 4, 8, 15, 16, 23, 42 };
-            int total=0;
-            bool found = false;
+            int currentAssignment = 5;
+            int[] studentScore = new int[10];
+            string currentStudentLetter = "";
 
-            foreach (int number in numbers)
+            int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
+            int[] AndrewScore = new int[] { 92, 89, 81, 96, 90 };
+            int[] emmaScore = new int[] { 90, 85, 87, 98, 68 };
+            int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
+            int[] beckyScores = new int[] { 92, 91, 90, 91, 92, 92, 92 };
+            int[] chrisScores = new int[] { 84, 86, 88, 90, 92, 94, 96, 98 };
+            int[] ericScores = new int[] { 80, 90, 100, 80, 90, 100, 80, 80 };
+            int[] gregorScore = new int[] { 91, 91, 91, 91, 91, 91 };
+
+            string[] studentName = { "Sophia", "Andrew", "Emma", "Logan","Becky","Chris","Eric","Gregor" };
+
+            foreach(string name in studentName)
             {
-                
+                string currentStudent = name;
 
-                total += number;
+                if (currentStudent == "Sophia")
+                    studentScore = sophiaScores;
+                else if (currentStudent == "Andrew")
+                    studentScore = AndrewScore;
+                else if (currentStudent == "Emma")
+                    studentScore = emmaScore;
+                else if (currentStudent == "Logan")
+                    studentScore = loganScores;
+                else if (currentStudent == "Becky")
+                    studentScore = beckyScores;
+                else if (currentStudent == "Chris")
+                    studentScore = chrisScores;
+                else if ((currentStudent == "Eric"))
+                    studentScore = ericScores;
+                else if (currentStudent == "Gregor")
+                    studentScore = gregorScore;
+                else
+                    continue;
 
-                if (number == 42)
+                int sumAssignmentScores = 0;
+                decimal currentStudentGrade = 0;
+
+                int gradeAssignment = 0;
+
+                foreach(int score in studentScore)
                 {
-                    found = true;
-
+                    gradeAssignment += 1;
+                    if(gradeAssignment <= currentAssignment)
+                    
+                        sumAssignmentScores += score;
+                    
+                    else
+                    
+                        sumAssignmentScores += score / 10;
+                    
                 }
 
+                currentStudentGrade = (decimal)sumAssignmentScores / currentAssignment;
+
+
+                
+
+                if (currentStudentGrade >= 97)
+                    currentStudentLetter = "A+";
+                else if (currentStudentGrade >= 93)
+                    currentStudentLetter = "A";
+                else if(currentStudentGrade>=90)
+                    currentStudentLetter = "A-";
+                else if (currentStudentGrade >=90)
+                    currentStudentLetter = "B+";
+                else if (currentStudentGrade >= 83)
+                    currentStudentLetter = "B";
+                else if(currentStudentGrade>=80)
+                    currentStudentLetter = "B-";
+                else if(currentStudentGrade >=77)
+                    currentStudentLetter = "C+";
+                else if(currentStudentGrade >=73)
+                    currentStudentLetter = "C";
+                else if(currentStudentGrade >=70)
+                    currentStudentLetter = "C-";
+                else if(currentStudentGrade >=67)
+                    currentStudentLetter = "D+";
+                else if(currentStudentGrade >=63)
+                    currentStudentLetter = "D";
+                else if (currentStudentGrade >= 60)
+                    currentStudentLetter = "D-";
+                else
+                    currentStudentLetter = "F";
+
+                Console.WriteLine($"{name}\t\t{currentStudentGrade}\t{currentStudentLetter}");
             }
 
-            if (found)
-            {
-                Console.WriteLine("Set contains 42");
+            Console.WriteLine("Press the enter key to continue");
 
-            }
 
-            Console.WriteLine($"Total: {total}");
+            
 
             Console.ReadLine();
         }
