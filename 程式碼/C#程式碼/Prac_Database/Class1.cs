b8e039de0c6698b40ae76351ccbce171acb86402
chenmax743 <chenmax38@gmail.com>
2024-10-30 14:21:51 +0800

    RN34

---
+++ 程式碼/C#程式碼/Prac_Database/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prac_Database
+{
+    class Student
+    {
+        public string studentName;
+
+        public int age;
+
+        public string Name
+        {
+            get { return studentName; }
+            set
+            {
+                if (studentName == null)
+                {
+                    studentName = value;
+                }
+                else
+                {
+                    Console.WriteLine("名稱不能流空");
+                }
+            }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set 
+            {
+                if(value>=0 && value<=120)
+                {
+                    age=value;
+                }
+                else
+                {
+                    Console.WriteLine("輸入有效名稱");
+                }
+            }
+
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"學生姓名:{Name},年齡:{Age}");
+        }
+
+        
+    }
+}
---
+++ 程式碼/C#程式碼/Prac_Database/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prac_Database
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Student student = new Student();
+
+            student.Name = "多拉A夢";
+            student.Age = 25;
+
+            student.Display();
+
+           
+
+            Console.ReadLine();
+        }
+    }
+}
